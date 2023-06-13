84e43430f954bd31e009b74679f7abe3c7b15a6f
SamMorganBrm <chaka1725@hotmail.com>
2023-06-13 15:54:14 -0700

    Update ReportGenerator.java

--- src/com/coderscampus/services/ReportGenerator.java
+++ src/com/coderscampus/services/ReportGenerator.java
@@ -7,20 +7,16 @@ import java.util.List;
 public class ReportGenerator {
 
     public void generateLowInventoryReport(ArrayList<Product> products) {
-        List<Product> lowInventory = new ArrayList<>();
+        //List<Product> lowInventory = new ArrayList<>();
 
         for (Product product : products) {
             double price = product.getPrice();
             int quantity = product.getQuantity();
-            if (price >= 0.99 && price <= 100 && quantity <= 20) {
-                lowInventory.add(product);
-            } else if (price >= 101.00 && quantity <= 10) {
-                lowInventory.add(product);
+            if ((price >= 0.99 && price <= 100 && quantity <= 20) || (price >= 101.00 && quantity <= 10)) {
+               
+                //lowInventory.add(product);
+                System.out.println(product.toString()+" Low inventory");
             }
-        }
-        
-        for (Product product : lowInventory) {
-            System.out.println(product.toString());
         }
     }
 }
